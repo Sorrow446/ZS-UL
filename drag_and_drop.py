f8940396b763c4faad3a83d646c4e04cd777ccdc
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2020-06-06 00:34:03 +0100

    Update drag_and_drop.py

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -1,7 +1,8 @@
 import sys
 import subprocess
 
-
-call = ['python', 'zs-ul.py', '-f']
+# Append other args here.
+call = ['python', 'zs-ul.py']
+call.append('-f')
 call.extend(sys.argv[1:])
 subprocess.Popen(call)
