3410e28a8d7fd9fd09bee65a4de0ca91b623d8a1
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2020-08-30 18:44:29 +0100

    Add files via upload

---
+++ zs-ul_with_prog_bar_experimental.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import sys
+import random
+import argparse
+
+import requests
+from tqdm import tqdm
+from requests_toolbelt import MultipartEncoder, MultipartEncoderMonitor
+
+
+def parse_prefs():
+	p = argparse.ArgumentParser()
+	p.add_argument(
+		'-f', '--files', 
+		nargs='+', required=True,
+		help='Paths of files to upload separated by a space.'
+	)
+	p.add_argument(
+		'-pv', '--private',
+		help='Set uploaded files to private.',
+		action='store_true'
+	)
+	p.add_argument(
+		'-p', '--proxy',
+		help='HTTPS only. <IP>:<port>.'
+	)
+	p.add_argument(
+		'-o', '--output',
+		help='Path of text file to write URLs to.'
+	)
+	p.add_argument(
+		'-n', '--no-wipe',
+		help='Don\'t wipe output text file before writing to it.',
+		action='store_true'
+	)
+	args = p.parse_args()
+	return args
+
+def dir_setup():
+	path = os.path.dirname(cfg.output)
+	try:
+		os.makedirs(path, exist_ok=True)
+	except FileNotFoundError:
+		pass
+
+def wipe_txt():
+	with open(cfg.output, 'w') as f:
+		pass
+
+def write_txt(fname, url):
+	with open(cfg.output, 'a+') as f:
+		f.write("{}\n{}\n".format(fname, url))
+	print("Wrote to: {}.".format(cfg.output))
+	
+def set_proxy():
+	s.proxies.update({'https': 'https://' + cfg.proxy})	
+
+def check_size(abs):
+	total = os.path.getsize(abs)
+	if total > 524288000:
+		raise Exception("File exceeds 500 MB limit.")
+	return total
+
+def upload(abs, fname):
+	server = random.randint(1, 120)
+	total = check_size(abs)
+	url = "https://www{}.zippyshare.com/upload".format(server)
+	f = open(abs, 'rb')
+	try:
+		data = {'file': (fname, f)}
+		pb = tqdm(total=total, unit='B', unit_scale=True, miniters=1)
+		multi = MultipartEncoder(fields=data)
+		monitor = MultipartEncoderMonitor(
+			multi, lambda multi: pb.update(monitor.bytes_read - pb.n))
+		if cfg.private:
+			data['private'] = "true"
+		else:
+			data['notprivate'] = "true"
+		s.headers.update({'Content-type': monitor.content_type})
+		r = s.post(url, data=monitor)
+		r.raise_for_status()
+	finally:
+		pb.close()
+		f.close()
+	return r.text
+
+def extract(html):
+	regex = (
+		r'onclick=\"this.select\(\);" value="(https://www\d{1,3}'
+		r'.zippyshare.com/v/[a-zA-Z\d]{8}/file.html)'
+	)
+	url = re.search(regex, html)
+	if not url:
+		raise Exception("Failed to extract URL.")
+	return url.group(1)
+
+def main(abs):
+	print(abs[:90])
+	fname = os.path.basename(abs)
+	html = upload(abs, fname)
+	url = extract(html)
+	print(url)
+	if cfg.output:
+		write_txt(fname, url)
+
+if __name__ == '__main__':
+	try:
+		if hasattr(sys, 'frozen'):
+			os.chdir(os.path.dirname(sys.executable))
+		else:
+			os.chdir(os.path.dirname(__file__))
+	except OSError:
+		pass
+	s = requests.Session()
+	s.headers.update({
+		'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+					  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome"
+					  "/75.0.3770.100 Safari/537.36",
+		'Referer': "https://www.zippyshare.com/"
+	})
+	print("""
+ _____ _____     _____ __    
+|__   |   __|___|  |  |  |   
+|   __|__   |___|  |  |  |__ 
+|_____|_____|   |_____|_____|
+	""")
+	cfg = parse_prefs()
+	if cfg.output:
+		dir_setup()
+		if not cfg.no_wipe:
+			wipe_txt()
+	if cfg.proxy:
+		set_proxy()
+	total = len(cfg.files)
+	for num, abs in enumerate(cfg.files, 1):
+		print("\nFile {} of {}:".format(num, total))
+		try:
+			main(abs)
+		except Exception as e:
+			print("Failed.")
+			print("{}: {}".format(e.__class__.__name__, e))
